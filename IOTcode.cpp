1dfc78ea83b2ad1faf28537b11597a6545238373
vadsokol <62573164+vadsokol@users.noreply.github.com>
2020-06-06 09:12:38 +0300

    Update IOTcode.cpp

--- IOTcode.cpp
+++ IOTcode.cpp
@@ -1,73 +1,73 @@
-// библиотека для работы программного Serial
+// Р±РёР±Р»РёРѕС‚РµРєР° РґР»СЏ СЂР°Р±РѕС‚С‹ РїСЂРѕРіСЂР°РјРјРЅРѕРіРѕ Serial
 #include <SoftwareSerial.h>
-
-// создаём объект для работы с программным Serial
-// и передаём ему пины TX и RX
+ 
+// СЃРѕР·РґР°С‘Рј РѕР±СЉРµРєС‚ РґР»СЏ СЂР°Р±РѕС‚С‹ СЃ РїСЂРѕРіСЂР°РјРјРЅС‹Рј Serial
+// Рё РїРµСЂРµРґР°С‘Рј РµРјСѓ РїРёРЅС‹ TX Рё RX
 SoftwareSerial mySerial(8, 9);
-
-// serial-порт к которому подключён Wi-Fi модуль
+ 
+// serial-РїРѕСЂС‚ Рє РєРѕС‚РѕСЂРѕРјСѓ РїРѕРґРєР»СЋС‡С‘РЅ Wi-Fi РјРѕРґСѓР»СЊ
 #define WIFI_SERIAL    mySerial
 
-const int ldrPin = A0;
+const int ldrPin = A0; 
 const int ledPin1 = 4;
 const int ledPin2 = 3;
 const int ledPin3 = 2;
 
-int ldrValue = 0;
-int ldrlevel1 = 500;
-int ldrlevel2 = 600;
-int ldrlevel3 = 700;
+int ldrValue = 0; 
+int ldrlevel1=500; 
+int ldrlevel2=600;
+int ldrlevel3=700;
 
 void setup()
 {
-    // открываем последовательный порт для мониторинга действий в программе
-    // и передаём скорость 9600 бод
-    Serial.begin(9600);
-    while (!Serial) {
-        // ждём, пока не откроется монитор последовательного порта
-        // для того, чтобы отследить все события в программе
-    }
-    Serial.print("Serial init OK\r\n");
-    // открываем Serial-соединение с Wi-Fi модулем на скорости 115200 бод
-    WIFI_SERIAL.begin(115200);
-    pinMode(ldrValue, INPUT);
-    // порты лампочек
-    pinMode(ledPin1, OUTPUT);
-    pinMode(ledPin2, OUTPUT);
-    pinMode(ledPin3, OUTPUT);
+  // РѕС‚РєСЂС‹РІР°РµРј РїРѕСЃР»РµРґРѕРІР°С‚РµР»СЊРЅС‹Р№ РїРѕСЂС‚ РґР»СЏ РјРѕРЅРёС‚РѕСЂРёРЅРіР° РґРµР№СЃС‚РІРёР№ РІ РїСЂРѕРіСЂР°РјРјРµ
+  // Рё РїРµСЂРµРґР°С‘Рј СЃРєРѕСЂРѕСЃС‚СЊ 9600 Р±РѕРґ
+  Serial.begin(9600);
+  while (!Serial) {
+  // Р¶РґС‘Рј, РїРѕРєР° РЅРµ РѕС‚РєСЂРѕРµС‚СЃСЏ РјРѕРЅРёС‚РѕСЂ РїРѕСЃР»РµРґРѕРІР°С‚РµР»СЊРЅРѕРіРѕ РїРѕСЂС‚Р°
+  // РґР»СЏ С‚РѕРіРѕ, С‡С‚РѕР±С‹ РѕС‚СЃР»РµРґРёС‚СЊ РІСЃРµ СЃРѕР±С‹С‚РёСЏ РІ РїСЂРѕРіСЂР°РјРјРµ
+  }
+  Serial.print("Serial init OK\r\n");
+  // РѕС‚РєСЂС‹РІР°РµРј Serial-СЃРѕРµРґРёРЅРµРЅРёРµ СЃ Wi-Fi РјРѕРґСѓР»РµРј РЅР° СЃРєРѕСЂРѕСЃС‚Рё 115200 Р±РѕРґ
+  WIFI_SERIAL.begin(115200);
+  pinMode(ldrValue, INPUT);
+  // РїРѕСЂС‚С‹ Р»Р°РјРїРѕС‡РµРє
+  pinMode(ledPin1, OUTPUT);
+  pinMode(ledPin2, OUTPUT);
+  pinMode(ledPin3, OUTPUT);
 }
-
+ 
 void loop()
 {
-    // если приходят данные из Wi-Fi модуля - отправим их в порт компьютера
-    if (WIFI_SERIAL.available()) {
-        Serial.write(WIFI_SERIAL.read());
-    }
-    // если приходят данные из компьютера - отправим их в Wi-Fi модуль
-    if (Serial.available()) {
-        WIFI_SERIAL.write(Serial.read());
-    }
-    ldrValue = analogRead(ldrPin);
-    if (ldrValue < ldrlevel1) {
-        digitalWrite(ledPin3, HIGH);
-        Serial.print("Light level - dark: ");
-        Serial.println(ldrValue);
-    }
-    else if (ldrValue < ldrlevel2) {
-        digitalWrite(ledPin3, LOW);
-        digitalWrite(ledPin2, HIGH);
-        Serial.print("Light level - dim: ");
-    }
-    else if (ldrValue < ldrlevel3) {
-        digitalWrite(ledPin2, LOW);
-        digitalWrite(ledPin1, HIGH);
-        Serial.print("Light level - bright: ");
-        Serial.print(ldrValue);
-    }
-    else {
-        digitalWrite(ledPin1, LOW);
-        Serial.println("Okay");
-    }
-
-    delay(250);
+  // РµСЃР»Рё РїСЂРёС…РѕРґСЏС‚ РґР°РЅРЅС‹Рµ РёР· Wi-Fi РјРѕРґСѓР»СЏ - РѕС‚РїСЂР°РІРёРј РёС… РІ РїРѕСЂС‚ РєРѕРјРїСЊСЋС‚РµСЂР°
+  if (WIFI_SERIAL.available()) {
+  	Serial.write(WIFI_SERIAL.read());
+  }
+  // РµСЃР»Рё РїСЂРёС…РѕРґСЏС‚ РґР°РЅРЅС‹Рµ РёР· РєРѕРјРїСЊСЋС‚РµСЂР° - РѕС‚РїСЂР°РІРёРј РёС… РІ Wi-Fi РјРѕРґСѓР»СЊ
+  if (Serial.available()) {
+  	WIFI_SERIAL.write(Serial.read());
+  }
+  ldrValue = analogRead(ldrPin);
+  if (ldrValue < ldrlevel1) {
+    digitalWrite(ledPin3, HIGH);
+    Serial.print("Light level - dark: ");
+    Serial.println(ldrValue);
+  }
+  else if (ldrValue < ldrlevel2) {
+    digitalWrite(ledPin3, LOW);
+    digitalWrite(ledPin2, HIGH);
+    Serial.print("Light level - dim: ");
+  }
+  else if (ldrValue < ldrlevel3) {
+    digitalWrite(ledPin2, LOW);
+    digitalWrite(ledPin1, HIGH);
+    Serial.print("Light level - bright: ");
+    Serial.print(ldrValue);
+  }
+  else {
+    digitalWrite(ledPin1, LOW);
+    Serial.println("Okay");
+  }
+  
+  delay(250);
 }
